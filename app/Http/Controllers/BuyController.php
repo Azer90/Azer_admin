99c90f672c0b72852483b2a49f2d80592749a099
Azer90 <2168177860@qq.com>
2019-04-07 17:00:57 +0800

    update

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -60,8 +60,8 @@ class BuyController extends Controller
 
             $order = [
                 'out_trade_no' => $order_no,
-                //'total_amount' => $goods['price'],
-                'total_amount' => 0.01,
+                'total_amount' => $goods['price'],
+                //'total_amount' => 0.01,
                 'subject'      => $goods['name'],
                 'http_method'  => 'URL'
             ];
@@ -134,13 +134,14 @@ class BuyController extends Controller
             // 支付宝交易号：$data->trade_no
            //还需要验证appid 和 写入 支付宝交易号 到数据库
             if($data->trade_status=='TRADE_SUCCESS'){
-                $out_trade_no=$data->out_trade_no;
-                PayOrder::where(['order_no'=>$out_trade_no])->update(['status'=>1]);
-                //$amount=PayOrder::where(['order_no'=>$data->out_trade_no])->value('amount');
-               // if((int)$amount==(int)($data->total_amount)){
 
-              //  }
-                Log::debug('Alipay notify', $data->all());
+                $amount=PayOrder::where(['order_no'=>$data->out_trade_no])->value('amount');
+
+                if((int)$amount==(int)($data->total_amount)){
+
+                    PayOrder::where(['order_no'=>$data->out_trade_no])->update(['status'=>1]);
+                }
+                //Log::debug('Alipay notify', $data->all());
             }
         } catch (\Exception $e) {
              //$e->getMessage();
