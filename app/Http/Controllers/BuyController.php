122cbb7b2a228f3b4d84de6b3ad4a2f6a50a4843
Azer90 <2168177860@qq.com>
2019-04-07 15:58:49 +0800

    update

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -134,10 +134,10 @@ class BuyController extends Controller
             // 支付宝交易号：$data->trade_no
            //还需要验证appid 和 写入 支付宝交易号 到数据库
             if($data->trade_status=='TRADE_SUCCESS'){
-                $amount=PayOrder::where(['order_no'=>$data->out_trade_no])->value('amount');
-                if((int)$amount==(int)($data->total_amount)){
+                //$amount=PayOrder::where(['order_no'=>$data->out_trade_no])->value('amount');
+               // if((int)$amount==(int)($data->total_amount)){
                     PayOrder::where(['order_no'=>$data->out_trade_no])->update(['status' => 1]);
-                }
+              //  }
 
             }
         } catch (\Exception $e) {
