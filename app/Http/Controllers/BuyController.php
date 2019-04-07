b9025988d2f7d21df597f6b92727b2e74c75e44f
Azer90 <2168177860@qq.com>
2019-04-07 14:19:43 +0800

    update

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -74,7 +74,8 @@ class BuyController extends Controller
             $order = [
                 'out_trade_no' => $order_no,
                 'body' =>  $goods['name'],
-                'total_fee'      => $goods['price']*100,
+                //'total_fee'      => $goods['price']*100,
+                'total_fee'      =>1,
             ];
             $result = Pay::wechat()->scan($order);
             $msg['message'] = $result["code_url"];
