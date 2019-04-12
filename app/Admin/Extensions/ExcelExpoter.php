4d777a763a9942548d513869363f19604f07b748
Azer90 <2168177860@qq.com>
2019-04-12 16:25:34 +0800

    update

--- app/Admin/Extensions/ExcelExpoter.php
+++ app/Admin/Extensions/ExcelExpoter.php
@@ -11,29 +11,31 @@ class ExcelExpoter extends AbstractExporter
     public function export()
     {
         $address=[];
-        if(!empty($this->getData())){
-            $ips=array_column($this->getData(),'ip');
+        $data=$this->getData();
+        if(!empty($data)){
+            $ips=array_column($data,'ip');
             $ips=array_unique($ips);
             foreach ($ips as $value){
                 $ipdata= IPController::find($value);
                 $address[$value]=$ipdata[1].$ipdata[2].$ipdata[3];
             }
+
         }
 
-        Excel::create('订单报表'.date('Ymd',time()), function($excel) use ($address){
+        Excel::create('订单报表'.date('Ymd',time()), function($excel) use ($address,$data){
 
-            $excel->sheet('订单报表', function($sheet)use ($address) {
+            $excel->sheet('订单报表', function($sheet)use ($address,$data) {
 
                 // 这段逻辑是从表格数据中取出需要导出的字段
 
 
-                $rows = collect($this->getData())->map(function ($item)use ($address) {
+                $rows = collect($data)->map(function ($item)use ($address) {
                     $data['id']=$item['id'];
                     $data['order_no']=$item['order_no'];
                     $data['payway']=$item['payway'];
                     $data['goods_name']=$item['goods_name'];
                     $data['amount']=$item['amount'];
-                   // $data['address']=$address[$item['ip']];
+                    $data['address']=$address[$item['ip']];
                     $data['email']=$item['email'];
                     $data['m_code']=$item['m_code'];
                     $data['created_at']=$item['created_at'];
@@ -59,5 +61,6 @@ class ExcelExpoter extends AbstractExporter
             });
 
         })->export('xls');
+
     }
 }
