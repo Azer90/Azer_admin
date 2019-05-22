bc0f0ba39af836d1655bca7683cb2e0747d3b1ed
Azer90 <2168177860@qq.com>
2019-05-22 20:23:35 +0800

    update

--- app/Common/Helper/functions.php
+++ app/Common/Helper/functions.php
@@ -23,3 +23,17 @@ function article_curl($api='',$urls=array()){
     $result = curl_exec($ch);
     return $result;
 }
+
+function Yoko_time($time =array()){
+    $stimestamp = strtotime($time['start_time']);
+    $etimestime = strtotime($time['end_time']);
+    //计算日期段内有多少天
+    $days = ($etimestime - $stimestamp)/86400+1;
+    //保存每天日期
+    $date = array();
+    /* for循环本周一到周日 */
+    for($i=0; $i<$days; $i++){
+        $date[] = date("Y-m-d",$stimestamp+(86400*$i));
+    }
+    return $date;
+}
--- vendor/encore/laravel-admin/src/Controllers/Dashboard.php
+++ vendor/encore/laravel-admin/src/Controllers/Dashboard.php
@@ -2,7 +2,9 @@
 
 namespace Encore\Admin\Controllers;
 
+use App\VisitorIp;
 use Encore\Admin\Admin;
+use Illuminate\Support\Facades\DB;
 
 class Dashboard
 {
@@ -44,60 +46,12 @@ class Dashboard
      */
     public static function extensions()
     {
-        $extensions = [
-            'helpers' => [
-                'name' => 'laravel-admin-ext/helpers',
-                'link' => 'https://github.com/laravel-admin-extensions/helpers',
-                'icon' => 'gears',
-            ],
-            'log-viewer' => [
-                'name' => 'laravel-admin-ext/log-viewer',
-                'link' => 'https://github.com/laravel-admin-extensions/log-viewer',
-                'icon' => 'database',
-            ],
-            'backup' => [
-                'name' => 'laravel-admin-ext/backup',
-                'link' => 'https://github.com/laravel-admin-extensions/backup',
-                'icon' => 'copy',
-            ],
-            'config' => [
-                'name' => 'laravel-admin-ext/config',
-                'link' => 'https://github.com/laravel-admin-extensions/config',
-                'icon' => 'toggle-on',
-            ],
-            'api-tester' => [
-                'name' => 'laravel-admin-ext/api-tester',
-                'link' => 'https://github.com/laravel-admin-extensions/api-tester',
-                'icon' => 'sliders',
-            ],
-            'media-manager' => [
-                'name' => 'laravel-admin-ext/media-manager',
-                'link' => 'https://github.com/laravel-admin-extensions/media-manager',
-                'icon' => 'file',
-            ],
-            'scheduling' => [
-                'name' => 'laravel-admin-ext/scheduling',
-                'link' => 'https://github.com/laravel-admin-extensions/scheduling',
-                'icon' => 'clock-o',
-            ],
-            'reporter' => [
-                'name' => 'laravel-admin-ext/reporter',
-                'link' => 'https://github.com/laravel-admin-extensions/reporter',
-                'icon' => 'bug',
-            ],
-            'redis-manager' => [
-                'name' => 'laravel-admin-ext/redis-manager',
-                'link' => 'https://github.com/laravel-admin-extensions/redis-manager',
-                'icon' => 'flask',
-            ],
-        ];
 
-        foreach ($extensions as &$extension) {
-            $name = explode('/', $extension['name']);
-            $extension['installed'] = array_key_exists(end($name), Admin::$extensions);
-        }
-
-        return view('admin::dashboard.extensions', compact('extensions'));
+        $time[] =date("Ymd", strtotime("-6 day"));  //获取当前周一
+        $time[]= date('Ymd',time());
+        $data=VisitorIp::select(DB::raw('count(ip) as count, time'))->whereBetween('time',$time)->groupBy('time')->get()->toArray();
+      
+        return view('admin::dashboard.extensions', compact('data'));
     }
 
     /**
