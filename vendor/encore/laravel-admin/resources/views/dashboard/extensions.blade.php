ced20e6b6dfbbd9c4b68b3514157df9536cbb9cb
Azer90 <2168177860@qq.com>
2019-05-22 21:48:55 +0800

    update

--- vendor/encore/laravel-admin/resources/views/dashboard/extensions.blade.php
+++ vendor/encore/laravel-admin/resources/views/dashboard/extensions.blade.php
@@ -1,21 +1,28 @@
 <canvas id="myChart" width="400" height="400"></canvas>
 <script>
+    var label= new Array();
+    var data= new Array();
+    @foreach ($data as $k=>$v)
+    label.push({{ $v['time'] }})
+    data.push({{ $v['count'] }})
+    @endforeach
     $(function () {
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: label,
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    label: '一周文章访问',
+                    data: data,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
                         'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 150, 62, 0.2)',
                     ],
                     borderColor: [
                         'rgba(255,99,132,1)',
@@ -23,7 +30,8 @@
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
                         'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 56, 42, 1)',
                     ],
                     borderWidth: 1
                 }]
