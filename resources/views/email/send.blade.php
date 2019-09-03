de3357b0df9368d26b304a5974703ae1ab41a487
Azer90 <2168177860@qq.com>
2019-09-03 10:24:02 +0800

    updata

--- resources/views/email/send.blade.php
+++ resources/views/email/send.blade.php
@@ -10,6 +10,7 @@
     &nbsp;您好，感谢使用晨光pdf转换器软件。<br/>
 晨光pdf转换器软件注册授权官方教程：http://www.ipdftoword.net/surpport/14<br/>
 晨光pdf转换器注册码采用【一机一码】,您的注册码是：{{$code}}<br/>
+直接复制注册码填写，点击【注册】后，提示注册成功，关闭软件重启使用。<br/>
 
 </span>
 <br/>
@@ -19,9 +20,11 @@
     Linda<br/>
     QQ:2638897163<br/>
     平台资源分享区：<br/>
+    AI智能生活：http://www.zaixianai.cn<br/>
+    PDF文件处理软件：http://www.ipdftoword.net<br/>
     文件数据恢复软件：https://www.superrecovery.cn<br/>
-    跨平台密码管理软件：http://www.1password.cn<br/>
-    个人企业文件备份软件：http://cn.filegee.com<br/>
+    个人企业文件备份软件：https://www.disksync.cn<br/>
+     跨平台密码管理软件：http://www.1password.cn<br/>
 </span>
 </body>
 </html>
